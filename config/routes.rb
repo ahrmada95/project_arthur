8da82b7592240cf7ec463f0e628b14123b119928
Ahnaf <ahrmada95@gmail.com>
2022-09-12 16:56:32 -0400

    made routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,50 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  get '/clients', to: 'clients#index'
+  get '/clients/:id', to: 'clients#show'
+  post '/clients', to: 'clients#create'
+  delete '/clients/:id', to: 'clients#destroy'
+  # patch '/clients/:id', to: 'clients#update'
+
+  get '/invoices', to: 'invoices#index'
+  get '/invoices/:id', to: 'invoices#show'
+  post '/invoices', to: 'invoices#create'
+  delete '/invoices/:id', to: 'invoices#destroy'
+  # patch '/invoices/:id', to: 'invoices#update'
+
+  get '/listings', to: 'listings#index'
+  get '/listings/:id', to: 'listings#show'
+  post '/listings', to: 'listings#create'
+  delete '/listings/:id', to: 'listings#destroy'
+  patch '/listings/:id', to: 'listing#update'
+
+  get '/reviews', to: 'reviews#index'
+  get '/reviews/:id', to: 'reviews#show'
+  post '/reviews', to: 'reviews#create'
+  delete '/reviews/:id', to: 'reviews#destroy'
+  # patch '/reviews/:id', to: 'reviews#update'
+
+  get '/sellers', to: 'sellers#index'
+  get '/sellers/:id', to: 'sellers#show'
+  post '/sellers', to: 'sellers#create'
+  delete '/sellers/:id', to: 'sellers#destroy'
+  # patch '/sellers/:id', to: 'sellers#update'
+
+  get '/transactions', to: 'transactions#index'
+  get '/transactions/:id', to: 'transactions#show'
+  post '/transactions', to: 'transactions#create'
+  delete '/transactions/:id', to: 'transactions#destroy'
+  # patch '/transactions/:id', to: 'transactions#update'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#destroy'
+  # patch '/users/:id', to: 'users#update'
+
+
+
 end
