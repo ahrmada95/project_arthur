22017805c557d586d44c2e8e46fbcc71cc446521
Ahnaf <ahrmada95@gmail.com>
2022-09-13 20:59:34 -0400

    cleaned up migrations by removing redunancies

--- db/migrate/20220913151633_add_tags_to_listing.rb
+++
@@ -1,5 +0,0 @@
-class AddTagsToListing < ActiveRecord::Migration[7.0]
-  def change
-    add_column :listings, :tags, :string, array: true, default: []
-  end
-end
--- db/migrate/20220913181845_remove_tags_from_listings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTagsFromListings < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :listings, :tags, :string
-  end
-end
