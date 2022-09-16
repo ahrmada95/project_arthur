b2da0107a22a4b796ed5c5d834a2d54d704576db
Ahnaf <ahrmada95@gmail.com>
2022-09-16 11:42:17 -0400

    totally not maine

--- client/src/components/ListingPage.js
+++ client/src/components/ListingPage.js
@@ -2,26 +2,22 @@ import '../styles/ListingPage.css'
 import { useState, useEffect } from 'react'
 import { Routes, Route, useParams } from 'react-router-dom';
 import ConfirmTransaction from './ConfirmTransaction'
-
-
-
-
 const ListingPage = ({setCartItems}) => {
+    window.scrollTo(0, 0);
     let { listingId } = useParams();
     const [isConfirming, setIsConfirming] = useState(false)
     const isPurchase = true
     const [listing, setListing] = useState({})
-
+    const [seller, setSeller] = useState({})
     useEffect(()=> {
         const getListingById = async() => {
             let req = await fetch(`http://localhost:3000/listings/${listingId}`)
             let res = await req.json()
-            setListing(res)
+            setListing(res.listing)
+            setSeller(res)
         }
-
         getListingById()
     }, [])
-
     const handleAddToCart =() => {
         let cart = JSON.parse(localStorage.getItem('cart'))
         if (cart == null){
@@ -32,18 +28,15 @@ const ListingPage = ({setCartItems}) => {
             setCartItems(prev => [...prev, listing])
             localStorage.setItem('cart', JSON.stringify(cart))
         }else {
-            console.log('already in cart')
         }
     }
-
-
-return(
+    return(
     <div id="listing-page">
         <div id='listing-top-wrapper'>
         <h1>{listing?.name}</h1>
         <div id='listing-top-container'>
             <div id='listing-img-container'>
-                <img src={'https://www.thebalancecareers.com/thmb/FDh8qXdM3B8zOvKiejYoz8zVrjY=/1500x1000/filters:fill(auto,1)/web-developer-job-description-salary-and-skills-2061841_final-edit-01-ac18fd4e99df46e990e4277a821faa0f.jpg'} alt={'title'}/>
+                <img src={listing?.images} alt={'title'}/>
             </div>
             <div id='listing-pricing-info'>
                 <h2>Always Affordable: ${listing?.price}</h2>
@@ -60,7 +53,6 @@ return(
         </div>
         </div>
         <div id='listing-bottom-wrapper'>
-
         <h2>About this listing:</h2>
             <p>{listing?.description}</p>
             <h2>About the creator:</h2>
@@ -69,15 +61,15 @@ return(
               <div id='listing-avatar-container'>
                 <img src={`https://avatars.dicebear.com/api/personas/${Math.floor(Math.random())}.svg`}/>
                 </div>
-                <p>{listing.seller?.id}</p>
-                <p id='listing-seller-rating'> {'⭐️'.repeat(listing?.seller?.rating)}{listing?.seller?.rating}</p>
+                <p>{seller?.first_name} {seller?.last_name}</p>
+                <p id='listing-seller-rating'> {':star:️'.repeat(seller?.rating)}{'★'.repeat(5 - seller?.rating)}</p>
+                <p id='seller-rating-num' >{seller?.rating}</p>
               </div>
-                <p>{listing?.seller?.bio}</p>
+                <p>{seller?.bio}</p>
             </div>
         </div>
-        {isConfirming&&< ConfirmTransaction setIsConfirming={setIsConfirming} isPurchase={isPurchase}/> }
+        {isConfirming&&< ConfirmTransaction setIsConfirming={setIsConfirming} isPurchase={isPurchase} listing={listing}/> }
     </div>
 )
 }
-
 export default ListingPage
